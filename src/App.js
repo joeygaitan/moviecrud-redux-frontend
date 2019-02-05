3645a72051c9adbe09ee20ac66477469dbae444e
Joey Gaitan <joeygaitan707@gmail.com>
2019-02-04 23:48:28 -0800

    added file structure for components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
-
+import {connect} from 'react-redux';
+import Home from './components/Home'
+import Movie from './components/Movie'
+import { addTodo } from './actions/index';
+import { bindActionCreators } from 'redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      
+      <BrowserRouter>
+      <div>
+        <Switch>
+        <Route exact path='/' component={Home}/>
+        <Route path='/:id' component={Movie}/>
+        </Switch>
+      </div>
+      </BrowserRouter>
     );
   }
 }
