9ef979c4bc965d14b45082ee3737b05bcd852774
Joey Gaitan <joeygaitan707@gmail.com>
2019-02-14 15:14:41 -0800

    Updates

---
+++ precache-manifest.33e502dd6f15e05ac897ebae732836f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3480c905a35b18a68cc",
+    "url": "/moviecrud-redux-frontend/static/js/main.b3480c90.chunk.js"
+  },
+  {
+    "revision": "acdd1010c558df52ce50",
+    "url": "/moviecrud-redux-frontend/static/css/1.82d86b1f.chunk.css"
+  },
+  {
+    "revision": "acdd1010c558df52ce50",
+    "url": "/moviecrud-redux-frontend/static/js/1.acdd1010.chunk.js"
+  },
+  {
+    "revision": "1db01b05da505fb07d21",
+    "url": "/moviecrud-redux-frontend/static/js/runtime~main.1db01b05.js"
+  },
+  {
+    "revision": "216de45028fee4150cebc308eb54dd05",
+    "url": "/moviecrud-redux-frontend/index.html"
+  }
+];
